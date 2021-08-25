4587b3ce17836f152aee948a3a9134fcdae00848
npwi223 <69438684+npwi223@users.noreply.github.com>
2021-08-25 16:07:04 -0400

    Update poke_colors.R

--- poke_colors.R
+++ poke_colors.R
@@ -2,30 +2,38 @@ library(colordistance)
 library(ggplot2)
 library(stringr)
 
-# Set your working directory to where the png images are located
+# Set your working directory to where the png images are located (remove comment below this)
+# setwd("User/file/path/here")
 # Identify the number of the pokemon based on the filename of the png
 # Find K-means clusters
-kmeans.clusters <- colordistance::getKMeanColors("150.png", # Change the file name here to adjust which pokemon you are using for your color palette
-                                                 n = 3,     # Change the number of colors you need in your color palette here
+
+poke_colors <- function(NPD_Number, ncolors) {
+
+NPD_Number <- ifelse(NPD_Number %>% str_length() == 1, paste0(0, 0, NPD_Number), NPD_Number)
+NPD_Number <- ifelse(NPD_Number %>% str_length() == 2, paste0(0, NPD_Number), NPD_Number)
+
+kmeans.clusters <- colordistance::getKMeanColors(paste0(NPD_Number,".png"),
+                                                 n = ncolors,     
                                                  plotting = FALSE)
 color_clusters <- colordistance::extractClusters(kmeans.clusters)
-clusters <- color_clusters
-hex <- function(clusters){
+
+hex <- function(color_clusters){
   dec2hex <-
     function(d){
       as.character(as.hexmode(d))}
-  number <- nrow(clusters)
-  r <- sapply(clusters[,1], function(x) dec2hex(round(x * 255)))
-  g <- sapply(clusters[,2], function(x) dec2hex(round(x * 255)))
-  b <- sapply(clusters[,3], function(x) dec2hex(round(x * 255)))
-  r <- ifelse(r %>% str_length() != c(2, 2, 2), paste0(0, r), r)
-  g <- ifelse(g %>% str_length() != c(2, 2, 2), paste0(0, g), g)
-  b <- ifelse(b %>% str_length() != c(2, 2, 2), paste0(0, b), b)
+  number <- nrow(color_clusters)
+  r <- sapply(color_clusters[,1], function(x) dec2hex(round(x * 255)))
+  g <- sapply(color_clusters[,2], function(x) dec2hex(round(x * 255)))
+  b <- sapply(color_clusters[,3], function(x) dec2hex(round(x * 255)))
+  r <- ifelse(r %>% str_length() != 2, paste0(0, r), r)
+  g <- ifelse(g %>% str_length() != 2, paste0(0, g), g)
+  b <- ifelse(b %>% str_length() != 2, paste0(0, b), b)
   return(paste0("#", toupper(r), toupper(g), toupper(b)))
 } 
-hex(color_clusters)
+return(hex(color_clusters))
+}
 
-              # Example plot of Iris with Mewtwo colors
+# Example plot of Iris with Mewtwo colors
 ggplot(iris, aes(x = Petal.Length, 
                  y = Petal.Width, 
                  size = Petal.Length*Petal.Width, 
@@ -35,4 +43,6 @@ ggplot(iris, aes(x = Petal.Length,
   theme(legend.position = "none",
         axis.text = element_blank(),
         axis.title = element_blank()) +
-  scale_color_manual(values = hex(color_clusters)) # Your color palette is updated in scale_color_manual
+  scale_color_manual(values = poke_colors("150", 3)) # Choose the number of the pokemon you want your color palette based on (in quotes)
+                                                     # Choose the number of colors you need (not in quotes)
+                                                  # Your color palette is updated in scale_color_manual
